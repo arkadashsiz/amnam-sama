700e294e297b9de320de8c899514e65d33ec1d67
arkadashsiz <applest.ariamoosavi@gmail.com>
2024-01-15 18:08:29 +0330

    initial structure of project and reused code

---
+++ functions.h
@@ -0,0 +1,251 @@
+//compares two chars
+int comp(char big, char small){
+    if (big<small)
+    {
+        return 1;
+    }
+    else if (big==small)
+    {
+        return 0;
+    }
+    
+    else{
+        return -1;
+    }
+}
+//compares two word with known length
+int comp_word(char *A,char *B,int length){
+    int res = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        if(comp(*(A+i),*(B+i))==0){
+            //
+        }
+        else if (comp(*(A+i),*(B+i))==1)
+        {
+            res=-1;
+            break;
+        }
+        else{
+            res = -1;
+            break;
+        }
+        
+    }
+    
+    return res;
+
+}
+//extracts int from string
+int decipher(int length,char* string){
+    int out_num=0;
+    int pow =1;
+    
+    for (int i = length-2; i >=0; i--)
+    {
+        switch (*(string+i))
+        {
+        case '0':
+
+            break;
+        case '1':
+            out_num+=pow;
+            break;
+        case '2':
+            out_num+=pow*2;
+            break;
+        case '3':
+            out_num+=pow*3;
+            break;
+        case '4':
+            out_num+=pow*4;
+            break;
+        case '5':
+            out_num+=pow*5;
+            break;
+        case '6':
+            out_num+=pow*6;
+            break;
+        case '7':
+            out_num+=pow*7;
+            break;
+        case '8':
+            out_num+=pow*8;
+            break;
+        case '9':
+            out_num+=pow*9;
+            break;
+        default:
+            out_num=-1;
+            break;
+        
+        }
+        
+        if (out_num==-1)
+        {
+            break;
+        }
+        
+        pow*=10;
+    }
+    
+    
+    return out_num;
+}
+//extracts float from string(kinda)
+float decipher_float(char *input,int length){
+    float out_num=0;
+    float pow=10;
+    if (length==1)
+    {
+        pow=1;
+    }
+    
+    if (*(input+1)=='.')
+    {
+        pow=1;
+        
+    }
+    else if (*(input+2)=='.')
+    {
+        pow =10;
+    }
+    for (int i = 0; i < length; i++)
+    {
+        if (*(input+i)=='.')
+        {
+            continue;
+        }
+        
+        switch (*(input+i))
+        {
+        case '0':
+
+            break;
+        case '1':
+            out_num+=pow;
+            break;
+        case '2':
+            out_num+=pow*2;
+            break;
+        case '3':
+            out_num+=pow*3;
+            break;
+        case '4':
+            out_num+=pow*4;
+            break;
+        case '5':
+            out_num+=pow*5;
+            break;
+        case '6':
+            out_num+=pow*6;
+            break;
+        case '7':
+            out_num+=pow*7;
+            break;
+        case '8':
+            out_num+=pow*8;
+            break;
+        case '9':
+            out_num+=pow*9;
+            break;
+        default:
+            out_num=-1;
+            break;
+        
+        }
+        pow/=10;
+    }
+    return out_num;
+
+}
+//size of a word with known start position
+int word_size(char *A,int start_pos){
+    int i=0;
+    while (1)
+    {
+        
+        if (*(A+i+start_pos)==' '||*(A+i+start_pos)=='\n'||*(A+i+start_pos)=='\0')
+        {
+            break;
+        }
+        
+        i++;
+    }
+    return i;
+}
+//start position of a word knowing start position of another word
+// and word distance between them
+int word_pos(char *A,int start_pos,int amount){
+    int ret_val=start_pos;
+    for (int i = 0; i < amount; i++)
+    {
+        ret_val+=word_size(A,ret_val)+1;
+    }
+    return ret_val;
+
+}
+//takes a(string of all commands) and b(start position of each command in string a)
+// and checks if string ((input)) is among them and returns the index
+int switch_input(int size_of_string_a,char* input,char*a,int*b){
+    int ret_val=-1;
+    for (int i = 0; i < size_of_string_a-1; i++)
+    {
+        if (comp_word(input,(a+b[i]),word_size(a,b[i]))==0)
+        {
+            ret_val=i;
+            break;
+        }
+        
+    }
+    return ret_val;
+
+}
+//copies string 1 to string 2
+void copy(char* copy_to,char* copy_from,int length){
+    for (int i = 0; i < length; i++)
+    {
+        *(copy_to+i)=*(copy_from+i);
+    }
+    
+
+
+}
+//empties a string(turns everything to ' ')
+void empty_str(char* a,int length){
+
+for (int i = 0; i < length; i++)
+{
+    *(a+i)=' ';
+}
+
+}
+//number of words split by space,new line,endline
+int num_words(char* a){
+    int temp_val=0;
+    int i=0;
+    int was_space=0;
+    while (1)
+    {
+        
+        if (was_space==0&&(*(a+i)==' '||*(a+i)=='\0'||*(a+i)=='\n'))
+        {
+            temp_val++;
+            was_space=1;
+        }
+        else if (*(a+i)!=' ')
+        {
+            was_space=0;
+        }
+        if (*(a+i)=='\0'||*(a+i)=='\n')
+        {
+            break;
+        }
+        i++;
+    }
+    return temp_val;
+    
+}
+
+//above functions are reused from HW's
